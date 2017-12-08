f2a8a2cd1f87f9b299776db90861b7f1b29d8321
Tim <tbrandle53@gmail.com>
2017-12-08 14:25:04 -0700

    will correctly blur on hover

--- script.js
+++ script.js
@@ -4,4 +4,39 @@ $('#nav-icon, .nav-link').click(function () {
   $('.nav-menu').toggleClass('hidden')
 });
 
+$(".title-content").on("mouseenter", function () {
+  $(this).siblings(".kiss-background").css({
+        'filter': 'blur(2.5px)',
+        '-webkit-filter': 'blur(2.5px)',
+        '-moz-filter': 'blur(2.5px)',
+        '-o-filter': 'blur(2.5px)',
+        '-ms-filter': 'blur(2.5px)'
+      });
+  $(this).siblings(".kiss-foreground").css({
+        'filter': 'blur(0px)',
+        '-webkit-filter': 'blur(0px)',
+        '-moz-filter': 'blur(0px)',
+        '-o-filter': 'blur(0px)',
+        '-ms-filter': 'blur(0px)'
+      });
+});
+
+$(".title-content").on("mouseleave", function () {
+  $(this).siblings(".kiss-foreground").css({
+        'filter': 'blur(2.5px)',
+        '-webkit-filter': 'blur(2.5px)',
+        '-moz-filter': 'blur(2.5px)',
+        '-o-filter': 'blur(2.5px)',
+        '-ms-filter': 'blur(2.5px)'
+      });
+  $(this).siblings(".kiss-background").css({
+        'filter': 'blur(0px)',
+        '-webkit-filter': 'blur(0px)',
+        '-moz-filter': 'blur(0px)',
+        '-o-filter': 'blur(0px)',
+        '-ms-filter': 'blur(0px)'
+      });
+});
+
+
 $(document).ready(function () { })
